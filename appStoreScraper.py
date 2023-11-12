8d09e0b90e51a52e2ec9c18df7b79fcbdfdaf25f
jonas <jonastraweek@yahoo.com>
2023-11-12 00:47:25 -0600

    Changed app scraper to take 2000 and added minecraft reviews.

--- appStoreScraper.py
+++ appStoreScraper.py
@@ -2,8 +2,9 @@ from app_store_scraper import AppStore
 import os
 import sys
 import csv
-import datetime
 
+minecraft = AppStore(country="nz", app_name="minecraft")
+minecraft.review(how_many=20)
 
 #print(minecraft.reviews)
 #print(minecraft.reviews_count)
@@ -25,25 +26,23 @@ else:
 for appAddr in apps:
 	try:
 		print("Fetching reviews for "+str(appAddr))
-		lines= 0
-		result = AppStore(country="us", app_name=appAddr)
-		while(lines < 10):
-			
-			result.review(how_many=20)
-			
+		
 
-			with open(str(outputFolder)+"/"+str(appAddr)+'.csv', 'w', newline='', encoding="utf-8") as file:
-				writer = csv.writer(file)
-				field = ["Score", "Date", "Content","Title"]
-				
-				writer.writerow(field)
-				for rev in result.reviews:
-					
-					#Removes line breaks from title and review to avoid processing errors
-					if(rev['date']<datetime.datetime(2017, 5, 18, 10, 59, 42, 518352)):
-						writer.writerow([rev['rating'], rev['date'],rev['review'].replace("\n", " "),rev['title'].replace("\n", " ")])
-						lines+=1
+		result = AppStore(country="us", app_name=appAddr)
+		result.review(how_many=2000)
+		
+
+		with open(str(outputFolder)+"/"+str(appAddr)+'.csv', 'w', newline='', encoding="utf-8") as file:
+			writer = csv.writer(file)
+			field = ["Score", "Date", "Content","Title"]
 			
+			writer.writerow(field)
+			for rev in result.reviews:
+				
+				#Removes line breaks from title and review to avoid processing errors
+				writer.writerow([rev['rating'], rev['date'],rev['review'].replace("\n", " "),rev['title'].replace("\n", " ")])
+   
+		
 	except Exception as e:
 		print("ERROR")
 		print(e)
--- csvTools.py
+++ csvTools.py
@@ -1,25 +1,54 @@
 import csv
 
+def findNewestAndOldest(reviewDict):
+	
+	max =reviewDict[0]['Date']
+	min =reviewDict[0]['Date']
+	for row in reviewDict:
+		
+		if(row['Date']<min):
+			min = row['Date']
+		if(row['Date']>max):
+			max = row['Date']
+	print("Max = "+str(max)+" Min = "+str(min))
+	return max , min
 
-def csvToDict(fileName):
-    reviewList=[]
-    with open(fileName, newline='') as csvfile:
+def csvToDict(fileName,apple = True):
+	reviewList=[]
+  
+	spamreader = csv.reader(fileName, delimiter=',', quotechar='|')
 
-        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+	skipFirst=True
+	for row in spamreader:
+		if(not skipFirst):
+			if(not apple):
+				thisdict =	{
+					'score': row[0],
+					'at': row[1],
+					'content': row[2]
+					}
+			else:
+				thisdict =	{
+					'Score': row[0],
+					'Date': row[1],
+					'Content': row[2],
+					'Title': row[3]
 
-        skipFirst=True
-        for row in spamreader:
-            if(not skipFirst):
-                 thisdict =    {
-                      'score': row[0],
-                      'at': row[1],
-                      'content': row[2]
-                    }
-                 reviewList.append(thisdict)
-            skipFirst=False
-            
-    return reviewList
-    
-    
+					}
+			reviewList.append(thisdict)
+		skipFirst=False
+			
+	return reviewList
+	
+	
+	
 def groupByDate(fileName,buckets):
-    reviewList = csvToDict(fileName)
+	reviewList = csvToDict(fileName)
+
+
+outputFolder = 'outputs_Apple'
+fileNameTest='cards-and-castles'
+with open(str(outputFolder)+"/"+str(fileNameTest)+'.csv', 'r', newline='', encoding="utf-8") as file:
+	out = csvToDict(file)
+	
+	findNewestAndOldest(out)
